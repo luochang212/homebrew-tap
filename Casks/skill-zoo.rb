8da1d3f28a95eb569a350ae1da115e866e9c0640
luochang212 <33987852+luochang212@users.noreply.github.com>
2026-05-07 20:52:30 +0000

    skill-zoo 0.1.4

--- Casks/skill-zoo.rb
+++ Casks/skill-zoo.rb
@@ -1,14 +1,14 @@
 cask "skill-zoo" do
-  version "0.1.3"
+  version "0.1.4"
 
   on_arm do
     url "https://github.com/luochang212/skill-zoo/releases/download/v#{version}/Skill-Zoo-v#{version}-macOS-arm64.dmg"
-    sha256 "ada5f119ea11d87267f823caed666d668b90f4d5124bf43e4e0bade28df466d0"
+    sha256 "495fa3984bef7a73fdb9514393e96e0f1a75844b24e33192b4d8fb33483e3bdc"
   end
 
   on_intel do
     url "https://github.com/luochang212/skill-zoo/releases/download/v#{version}/Skill-Zoo-v#{version}-macOS-x64.dmg"
-    sha256 "b3d556f36b6fb5e4671dbc7abb98b4bbdd8a0965c529f7cc2ab7e8445d1bfb66"
+    sha256 "458196b1f2af7b4585cf4d58a90d132f85c747d6a53dddfa93234018f38e6930"
   end
 
   name "Skill Zoo"
