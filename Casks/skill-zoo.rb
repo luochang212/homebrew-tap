7d8e1a1146c17e0f6d6461b5d08d49e9faee5dda
Chang Luo <33987852+luochang212@users.noreply.github.com>
2026-05-25 05:49:27 +0800

    skill-zoo 0.2.2 (#21)

--- Casks/skill-zoo.rb
+++ Casks/skill-zoo.rb
@@ -1,8 +1,8 @@
 cask "skill-zoo" do
-  version "0.1.12"
+  version "0.2.2"
 
   url "https://github.com/luochang212/skill-zoo/releases/download/v#{version}/Skill-Zoo-v#{version}-macOS.dmg"
-  sha256 "430b5b833f07b1cbb6676134fe6b3835a09c71fa5d50ea2c60064f69df6e6c13"
+  sha256 "4b7f9918878a6681126667c5cb6d819bc5ae135dab38d7480b23de6727a14ff1"
 
   name "Skill Zoo"
   desc "Local GUI tool for managing AI Agent Skills"
